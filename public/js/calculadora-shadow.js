7fb4a12f4e95a2429e54c4a3a02e941e2aad99a3
Job <jobgoyesd@outlook.com>
2025-06-25 16:02:46 -0500

    4to. Actualización public/js/

--- public/js/calculadora-shadow.js
+++ public/js/calculadora-shadow.js
@@ -1,1 +1,38 @@
-alert("calculadora-shadow.js");
+class CalculadoraShadowDom extends HTMLElement {
+    constructor() {
+        super();
+        this.shadow = this.attachShadow({ mode: 'open'});
+
+        this.shadow.innerHTML = `
+        <link href="./public/lib/bootstrap-5.3.6-dist/css/bootstrap.min.css" rel="stylesheet">
+        <div class=" card p-4" style="width: 400px;">
+        <div class="mb-3">
+            <label class="from-label">Primer número</label>
+            <input type="number" id="numero1" class="from-control" placeholder="Escriba aquí">
+        </div>
+        <div class="mb-3">
+            <label class="from-label">Segundo número</label>
+            <input type="number" id="numero2" class="from-control" placeholder="Escriba aquí">
+        </div>
+        <div class="mb-3">
+            <label class="from-label">Operación a realizar</label>
+            <select id="operacionCalculadora" class="from-select">
+                <option value="suma">Suma</option>
+                <option value="resta">Resta</option>
+                <option value="multiplicacion">Multiplicación</option>
+                <option value="division">División</option>
+            </select>
+        </div>
+        <button id="realizarCalculo" class="btn btn-success w-100">Calcular resultado</button>
+        <div class="mt-3 alert alert-secondary" id="resultadoCalculadora" role="alert">
+            Resultado obtenido:
+        </div>
+        <hr>
+        <h5>Historial de operaciones:</h5>
+        <ul id="historialCalculadora" class="list-group list-group-flush"></ul>
+        </div>
+        `;
+    }
+}
+
+customElements.define('calculadora-shadow-dom', CalculadoraShadowDom);
